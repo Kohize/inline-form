26aaaecf629614e68b140b7d04d916bd049956f2
Kohize <kohi881@gmail.com>
2025-02-20 21:50:56 +0300

    email and postal validation

--- src/main.js
+++ src/main.js
@@ -1,13 +1,32 @@
 let emailField = document.querySelector('.email');
+let postalField = document.querySelector('.postal');
+
 const EMAIL_REGEXP =
   /^(([^<>()[\].,;:\s@"]+(\.[^<>()[\].,;:\s@"]+)*)|(".+"))@(([^<>()[\].,;:\s@"]+\.)+[^<>()[\].,;:\s@"]{2,})$/iu;
-console.log(emailField);
+const POSTAL_REGEXP = /^(\d(\s+)?){6}$/;
 
-const onInput = () => {
+const makeInputValid = (field, fieldStatus) => {
+  field.style.borderColor = 'green';
+  fieldStatus.innerHTML = '✔️';
+  fieldStatus.classList.remove('invalid');
+  fieldStatus.classList.add('valid');
+};
+
+const makeInputInvalid = (field, fieldStatus) => {
+  field.style.borderColor = 'red';
+  fieldStatus.innerHTML = `❌ You must enter a valid ${field.getAttribute(
+    'placeholder'
+  )}`;
+  fieldStatus.classList.remove('valid');
+  fieldStatus.classList.add('invalid');
+};
+
+const checkEmail = () => {
+  let emailFieldStatus = document.querySelector('.input__status--email');
   if (isEmailValid(emailField.value)) {
-    emailField.style.borderColor = 'green';
+    makeInputValid(emailField, emailFieldStatus);
   } else {
-    emailField.style.borderColor = 'red';
+    makeInputInvalid(emailField, emailFieldStatus);
   }
 };
 
@@ -15,4 +34,18 @@ const isEmailValid = (value) => {
   return EMAIL_REGEXP.test(value);
 };
 
-emailField.addEventListener('input', onInput);
+const checkPostal = () => {
+  let postalFieldStatus = document.querySelector('.input__status--postal');
+  if (isPostalValid(postalField.value)) {
+    makeInputValid(postalField, postalFieldStatus);
+  } else {
+    makeInputInvalid(postalField, postalFieldStatus);
+  }
+};
+
+const isPostalValid = (value) => {
+  return POSTAL_REGEXP.test(value);
+};
+
+emailField.addEventListener('input', checkEmail);
+postalField.addEventListener('input', checkPostal);
